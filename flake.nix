3245e5b5be3d200cbfc6200020945d0cd5ab8759
MattHandzel <matthewwalkosz@gmail.com>
2025-03-14 21:30:35 -0500

    adding setup.rs and lib.rs

--- flake.nix
+++ flake.nix
@@ -12,7 +12,35 @@
         overlays = [ (import rust-overlay) ];
       };
     in {
+
+      packages.${system}.default = pkgs.rustPlatform.buildRustPackage {
+        pname = "lifelog";
+        version = "0.1.0";
+        src = ./.;
+        cargoSha256 = "sha256-0000000000000000000000000000000000000000000000000000"; 
+
+        cargoLock = {
+          lockFile = ./Cargo.lock;
+        };
+
+        buildInputs = [
+          pkgs.pkg-config
+          pkgs.openssl
+          pkgs.alsa-lib
+        ];
+
+        nativeBuildInputs = [
+          pkgs.cmake
+        ];
+
+        meta = with pkgs.lib; {
+          description = "My Rust Project";
+          license = licenses.mit;
+          maintainers = [ maintainers.yourname ];
+        };
+      };
       devShells.${system}.default = pkgs.mkShell {
+
         packages = [
           (pkgs.rust-bin.stable.latest.default.override {
             extensions = [ "rust-src" "rust-analyzer" ];
---
+++ src/setup.rs
@@ -0,0 +1,5 @@
+pub fn setup(config: Config){
+    // load up the config, ensure all directories exist
+
+}
+
