cce511b544b9d52e685d1c8ed19b7a47e856d9f6
MattHandzel <matthewwalkosz@gmail.com>
2025-04-01 14:21:47 -0500

    updating flake and refactoring

--- flake.nix
+++ flake.nix
@@ -9,105 +9,105 @@
     nixpkgs,
     rust-overlay,
   }: let
-    system = "x86_64-linux";
-    pkgs = import nixpkgs {
-      inherit system;
-      overlays = [(import rust-overlay)];
-    };
+    systems = ["x86_64-linux" "x86_64-darwin" "aarch64-darwin"];
+    forAllSystems = nixpkgs.lib.genAttrs systems;
   in {
-    packages.${system} = {
-      lifelog-server = pkgs.rustPlatform.buildRustPackage {
+    packages = forAllSystems (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [(import rust-overlay)];
+      };
+
+      commonDeps = {
+        buildInputs = with pkgs;
+          [
+            pkg-config
+            openssl
+            sqlite
+          ]
+          ++ pkgs.lib.optionals (pkgs.stdenv.isLinux) [
+            alsa-lib
+            linuxPackages.v4l2loopback
+            v4l-utils
+            xorg.libX11
+            xorg.libXtst
+            xorg.libXi
+          ];
+
+        nativeBuildInputs = with pkgs;
+          [
+            pkg-config
+            cmake
+          ]
+          ++ pkgs.lib.optionals (pkgs.stdenv.isLinux) [
+            alsa-lib
+          ];
+      };
+
+      mkRustPackage = {
+        pname,
+        binName,
+      }:
+        pkgs.rustPlatform.buildRustPackage {
+          inherit pname;
+          version = "0.1.0";
+          src = ./.;
+          cargoLock.lockFile = ./Cargo.lock;
+          cargoBuildFlags = ["--bin" binName];
+
+          buildInputs = commonDeps.buildInputs;
+          nativeBuildInputs = commonDeps.nativeBuildInputs;
+
+          meta = with pkgs.lib; {
+            description = "A project to log various sources of data for your lifelog";
+            license = licenses.mit;
+            maintainers = [maintainers.MattHandzel];
+          };
+        };
+    in {
+      lifelog-server = mkRustPackage {
         pname = "lifelog-server";
-        version = "0.1.0";
-        src = ./.;
-
-        cargoLock = {
-          lockFile = ./Cargo.lock;
-        };
-
-        buildInputs = with pkgs; [
-          pkg-config
-          openssl
-          alsa-lib
-          sqlite
-          linuxPackages.v4l2loopback
-          v4l-utils
-          xorg.libX11
-          xorg.libXtst
-          xorg.libXi
-        ];
-
-        cargoBuildFlags = ["--bin" "lifelog-server"];
-
-        nativeBuildInputs = with pkgs; [
-          pkg-config
-          cmake
-          alsa-lib
-        ];
-
-        meta = with pkgs.lib; {
-          description = "A project to log various sources of data for your lifelog";
-          license = licenses.mit;
-          maintainers = [maintainers.MattHandzel];
-        };
+        binName = "lifelog-server";
       };
-      lifelog-logger = pkgs.rustPlatform.buildRustPackage {
+      lifelog-logger = mkRustPackage {
         pname = "lifelog-logger";
-        version = "0.1.0";
-        src = ./.;
-
-        cargoLock = {
-          lockFile = ./Cargo.lock;
-        };
-
-        buildInputs = with pkgs; [
-          pkg-config
-          openssl
-          alsa-lib
-          sqlite
-          linuxPackages.v4l2loopback
-          v4l-utils
-          xorg.libX11
-          xorg.libXtst
-          xorg.libXi
-        ];
-
-        cargoBuildFlags = ["--bin" "lifelog-logger"];
-
-        nativeBuildInputs = with pkgs; [
-          pkg-config
-          cmake
-          alsa-lib
-        ];
-
-        meta = with pkgs.lib; {
-          description = "A project to log various sources of data for your lifelog";
-          license = licenses.mit;
-          maintainers = [maintainers.MattHandzel];
-        };
+        binName = "lifelog-logger";
       };
-    };
-    devShells.${system}.default = pkgs.mkShell {
-      PKG_CONFIG_PATH = "${pkgs.alsa-lib}/lib/pkgconfig";
+    });
 
-      packages = with pkgs; [
-        (rust-bin.stable.latest.default.override {
-          extensions = ["rust-src" "rust-analyzer"];
-        })
-        openssl
-        glibc
-        linuxPackages.v4l2loopback
-        v4l-utils
-        libxkbcommon
-        pkg-config
-        alsa-lib
-        grim
-        xorg.libX11
-        slurp
-        xorg.libXtst
-        xorg.libXi
-        sqlite
-      ];
-    };
+    devShells = forAllSystems (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [(import rust-overlay)];
+      };
+    in {
+      default = pkgs.mkShell {
+        PKG_CONFIG_PATH =
+          pkgs.lib.optionalString pkgs.stdenv.isLinux
+          "${pkgs.alsa-lib}/lib/pkgconfig";
+
+        packages = with pkgs;
+          [
+            (rust-bin.stable.latest.default.override {
+              extensions = ["rust-src" "rust-analyzer"];
+            })
+            openssl
+            sqlite
+            pkg-config
+            cmake
+          ]
+          ++ pkgs.lib.optionals pkgs.stdenv.isLinux [
+            linuxPackages.v4l2loopback
+            v4l-utils
+            libxkbcommon
+            alsa-lib
+            grim
+            xorg.libX11
+            slurp
+            xorg.libXtst
+            xorg.libXi
+          ];
+      };
+    });
   };
 }
