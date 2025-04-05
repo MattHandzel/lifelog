b016a79b1f7e7e33e13e574bec3a720dfc368a32
MattHandzel <matthewwalkosz@gmail.com>
2025-04-04 20:34:38 -0500

    refactoring the project by a lot!

---
+++ logger/src/modules/mod.rs
@@ -0,0 +1,13 @@
+// Export all modules
+pub mod camera;
+pub mod evdev_input_logger;
+pub mod hyprland;
+pub mod input_logger;
+pub mod logger;
+pub mod microphone;
+pub mod processes;
+pub mod screen;
+pub mod text_upload;
+pub mod wayland_input_logger;
+pub mod weather;
+
