1582a6cee4bd6b3bc09fd6765a75824720694fac
MattHandzel <handzelmatthew@gmail.com>
2026-03-03 19:51:35 -0600

    fix: disable well-known type compilation in interface to fix trait errors

--- interface/src-tauri/build.rs
+++ interface/src-tauri/build.rs
@@ -10,7 +10,6 @@ fn main() -> Result<(), Box<dyn Error>> {
         .build_client(true) // We need the client code
         .build_server(false) // We don't need server code in the interface
         .file_descriptor_set_path(out_dir.join("lifelog_descriptor.bin")) // Store the descriptor set
-        .compile_well_known_types(true) // Generate code for well-known types so serde derive can be applied
         .type_attribute(".", "#[derive(serde::Serialize, serde::Deserialize)]") // Add serde derive attributes to generated message types
         .compile_protos(
             &[
