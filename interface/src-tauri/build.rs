6ae355c1741f603427b574ceea886ed8f05d1fcf
MattHandzel <handzelmatthew@gmail.com>
2026-03-03 19:57:04 -0600

    fix: use selective serde derives in interface to fix trait bound issues

--- interface/src-tauri/build.rs
+++ interface/src-tauri/build.rs
@@ -10,8 +10,86 @@ fn main() -> Result<(), Box<dyn Error>> {
         .build_client(true) // We need the client code
         .build_server(false) // We don't need server code in the interface
         .file_descriptor_set_path(out_dir.join("lifelog_descriptor.bin")) // Store the descriptor set
-        .type_attribute(".", "#[derive(serde::Serialize, serde::Deserialize)]") // Add serde derive attributes to generated message types
-        .extern_path(".google.protobuf.Timestamp", "::pbjson_types::Timestamp")
+        .type_attribute(
+            "lifelog.Query",
+            "#[derive(serde::Serialize, serde::Deserialize)]",
+        )
+        .type_attribute(
+            "lifelog.LifelogDataKey",
+            "#[derive(serde::Serialize, serde::Deserialize)]",
+        )
+        .type_attribute(
+            "lifelog.ModalityDescriptor",
+            "#[derive(serde::Serialize, serde::Deserialize)]",
+        )
+        .type_attribute(
+            "lifelog.FieldDescriptor",
+            "#[derive(serde::Serialize, serde::Deserialize)]",
+        )
+        .type_attribute(
+            "lifelog.SystemConfig",
+            "#[derive(serde::Serialize, serde::Deserialize)]",
+        )
+        .type_attribute(
+            "lifelog.CollectorConfig",
+            "#[derive(serde::Serialize, serde::Deserialize)]",
+        )
+        .type_attribute(
+            "lifelog.ServerConfig",
+            "#[derive(serde::Serialize, serde::Deserialize)]",
+        )
+        .type_attribute(
+            "lifelog.BrowserHistoryConfig",
+            "#[derive(serde::Serialize, serde::Deserialize)]",
+        )
+        .type_attribute(
+            "lifelog.ScreenConfig",
+            "#[derive(serde::Serialize, serde::Deserialize)]",
+        )
+        .type_attribute(
+            "lifelog.CameraConfig",
+            "#[derive(serde::Serialize, serde::Deserialize)]",
+        )
+        .type_attribute(
+            "lifelog.MicrophoneConfig",
+            "#[derive(serde::Serialize, serde::Deserialize)]",
+        )
+        .type_attribute(
+            "lifelog.ProcessesConfig",
+            "#[derive(serde::Serialize, serde::Deserialize)]",
+        )
+        .type_attribute(
+            "lifelog.HyprlandConfig",
+            "#[derive(serde::Serialize, serde::Deserialize)]",
+        )
+        .type_attribute(
+            "lifelog.WeatherConfig",
+            "#[derive(serde::Serialize, serde::Deserialize)]",
+        )
+        .type_attribute(
+            "lifelog.WifiConfig",
+            "#[derive(serde::Serialize, serde::Deserialize)]",
+        )
+        .type_attribute(
+            "lifelog.ClipboardConfig",
+            "#[derive(serde::Serialize, serde::Deserialize)]",
+        )
+        .type_attribute(
+            "lifelog.ShellHistoryConfig",
+            "#[derive(serde::Serialize, serde::Deserialize)]",
+        )
+        .type_attribute(
+            "lifelog.MouseConfig",
+            "#[derive(serde::Serialize, serde::Deserialize)]",
+        )
+        .type_attribute(
+            "lifelog.WindowActivityConfig",
+            "#[derive(serde::Serialize, serde::Deserialize)]",
+        )
+        .type_attribute(
+            "lifelog.KeyboardConfig",
+            "#[derive(serde::Serialize, serde::Deserialize)]",
+        )
         .compile_protos(
             &[
                 "../../proto/lifelog.proto",       // Path relative to build.rs
